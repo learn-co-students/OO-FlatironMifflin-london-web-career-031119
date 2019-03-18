0a58db04613667e2a003c83225b2413ec3b0fa48
github username <github email address>
2019-03-18 14:15:01 +0000

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,32 @@
 class Employee
+  attr_accessor :name, :salary, :manager
+
+  @@all = []
+
+  def initialize(name, salary, manager_name)
+    @name = name
+    @salary = salary
+    @manager_name = manager_name
+    @@all << self
+  end
+
+    def self.all
+      @@all
+    end
+
+  def self.paid_over(threshold)
+    Employee.all.select {|e| e.salary >= threshold}
+  end
+
+  def self.find_by_department(department)
+    Employee.all.find {|e| e.manager.department == department}
+  end
+
+  def tax_bracket
+    range = (self.salary - 1000..self.salary+1000).to_a
+    selected = Employee.all.select {|e| range.include?(e.salary)}
+    selected.delete(self)
+    selected
+  end
 
 end
--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,35 @@
 class Manager
+  attr_accessor :name, :department, :age, :employee, :salary
+
+  @@all = []
+
+  def initialize(name, department, age)
+    @name = name
+    @department = department
+    @age = age
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def employees
+    Employee.all.select {|e| e.manager == self}
+  end
+
+  def hire_employee(employee, salary)
+    employees << Employee.new(employee, salary, self)
+  end
+
+  def self.all_departments
+    self.all.map {|m| m.department}
+  end
+
+  def self.average_age
+    ages = self.all.collect {|m| m.age}
+    age_sum = ages.inject(0){|sum, x| sum + x }
+    (age_sum / ages.size.to_f)
+  end
 
 end
