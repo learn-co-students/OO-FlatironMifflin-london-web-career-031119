294340a48038033e25068073fd48c295633e7729
Manon Jacquin <manon.jacquin@gmail.com>
2019-03-18 16:40:54 +0000

    Done.

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,34 @@
 class Manager
 
+  attr_accessor :name, :department, :age, :employees
+
+  @@all = []
+
+  def initialize(name, department, age, employees)
+    @name = name
+    @department = department
+    @age = age
+    @employees = employees
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def hire_employee(name, salary)
+    new_employee = Employee.new(name, salary, self)
+    employees << new_employee.name
+  end
+
+  def self.all_departments
+    Manager.all.collect {|dept| dept.department }
+  end
+
+  def self.average_age
+    total_managers = Manager.all.length
+    Manager.all.collect {|age| age.age }.sum / total_managers
+  end
+
 end
--- run.rb
+++ run.rb
@@ -4,7 +4,22 @@ require 'pry'
 
 
 #Test your code here
+manager1 = Manager.new('Mikael', 'law', 56, ['Anton', 'Anna', 'Mary'])
+manager2 = Manager.new('David', 'music', 44, ['Molly', 'Robert', 'David'])
+manager3 = Manager.new('Sarah', 'books', 23, ['Mikael', 'Peter', 'Gulnaz'])
+manager4 = Manager.new('Louise', 'human rights', 36, ['Harry', 'Justine', 'Julia'])
+
+employee1 = Employee.new('Anton', 900, 'Mikael')
+employee2 = Employee.new('Mary', 560, 'David')
+employee3 = Employee.new('Nicolas', 140, 'Sarah')
+employee4 = Employee.new('Julie', 1050 , 'Louise')
 
 
-binding.pry
+
+
+
+
+
+
+# binding.pry
 puts "done"
