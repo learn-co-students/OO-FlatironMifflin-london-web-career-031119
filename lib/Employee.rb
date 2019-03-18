11d81b6b711471963a62ea8c92a169476646e506
oli-gra <oliver.grant@gmail.com>
2019-03-18 17:57:48 +0000

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -15,10 +15,17 @@ def self.all
   @@all
 end
 
-def paid_over
+def self.paid_over(salary)
+  Employee.all.select{|employee|employee.salary.to_i > salary}
 end
 
-def find_by_department
+def self.find_by_department(department)
+  manager = Manager.all.find{|manager|manager.department == department}
+  Employee.all.find{|employee|employee.manager_name == manager}
+end
+
+def self.tax_bracket
+  Employee.all.select{|employee|employee.salary.to_i == Range.new(employee.salary.to_i-1000..employee.salary.to_i+1000) }
 end
 
 end
