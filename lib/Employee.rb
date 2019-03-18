3eaf31ae50da7046f9e81c9a0dabeab6e0d1a87e
“Ranjit <ranjit.saimbi@gmail.com>
2019-03-18 20:30:37 +0000

    Done

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,29 @@
 class Employee
+  attr_accessor :name, :salary, :manager_name
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.paid_over(number)
+    @@all.select {|employee| employee.salary > number}
+  end
+
+  def self.find_by_department(department)
+    manager = Manager.all.find {|manager| manager.department == department}
+    @@all.find {|employee| employee.manager_name == manager.name}
+  end
+
+   def initialize(name, salary, manager_name)
+     @name = name
+     @salary = salary
+     @manager_name = manager_name
+     @@all << self
+   end
+
+   def tax_bracket
+     @@all.select {|employee| self.salary + 1000 > employee.salary && self.salary - 1000 < employee.salary}
+   end
 
 end
--- run.rb
+++ run.rb
@@ -4,7 +4,16 @@ require 'pry'
 
 
 #Test your code here
+m1 = Manager.new("Ranjit", "Books", 29)
+m2 = Manager.new("Oli", "Computers", 33)
+m3 = Manager.new("Peter", "Science", 26)
 
+e1 = Employee.new("Myrto", 30000, "Ranjit")
+e2 = Employee.new("Kevin", 35000, "Oli")
+e3 = Employee.new("Riccardo", 40000, "Peter")
+e4 = Employee.new("Bob", 40001, "Peter")
+e5 = Employee.new("Matthew", 40002, "Peter")
+e6 = Employee.new("Guy", 40003, "Peter")
 
 binding.pry
 puts "done"
