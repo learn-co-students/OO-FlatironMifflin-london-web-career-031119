1c1e45fdc4c46deee7df52ddf7a88097b0d3b344
oli-gra <oliver.grant@gmail.com>
2019-03-18 17:01:15 +0000

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,24 @@
 class Employee
 
+@@all = []
+
+attr_reader :name, :salary, :manager_name, :department
+
+def initialize(name,salary,department,manager_name)
+  @name = name
+  @salary = salary
+  @manager_name = manager_name
+  @@all << self
+end
+
+def self.all
+  @@all
+end
+
+def paid_over
+end
+
+def find_by_department
+end
+
 end
