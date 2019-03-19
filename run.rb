e5680fce4d8c9c615d7c368d58ad3285658fd595
keoki33 <gwong323@yahoo.com>
2019-03-19 00:38:07 +0000

    done

--- run.rb
+++ run.rb
@@ -3,8 +3,33 @@ require_relative "lib/Employee"
 require 'pry'
 
 
-#Test your code here
+joe = Manager.new("Joe", "one", 62)
+  john = Manager.new("John", "two", 44)
+    mary = Manager.new("Mary", "three", 35)
+
+tim = joe.hire_employee("Tim", 10000)
+ronnie = joe.hire_employee("Ronnie", 10900)
+jason = joe.hire_employee("Jason", 9900)
+tina = mary.hire_employee("Tina", 5000)
+
+puts "--------------------------"
 
 
-binding.pry
+
+# p mary.employees
+
+# p Manager.all
+
+# p Manager.average_age
+
+# p Employee.find_by_department("one")
+
+# p tim.tax_bracket
+
+
+
+
+
+
+# binding.pry
 puts "done"
