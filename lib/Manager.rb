8f5888263d104489d4c0c7c5fdf83c84565ce3ec
github username <github email address>
2019-03-19 10:58:29 +0000

    submission

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,51 @@
 class Manager
 
+  attr_reader :name, :department, :age
+  @@all = []
+
+  def initialize(name, department, age)
+    @name = name
+    @department = department
+    @age = age
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
+  def employees
+    Employee.all.select {|employee| employee.manager_name == self}
+  end
+
+
+  def hire_employee(name, salary)
+    Employee.new(self, name, salary)
+  end
+
+
+  # def total_age
+  #   Manager.all.map {|manager| manager.age}.sum
+  # end
+  #
+  #
+  # def average_age
+  #   # total = total_age.inject(:+)
+  #   # total / total_age.length.to_f
+  #   total_age.sum / total_age.total
+  # end
+
+  def total_age
+    abc = Manager.all.map {|manager| manager.age}
+  end
+
+  def average_age
+    # total = total_age.inject(:+)
+    # total / total_age.length.to_f
+    total_age.sum / total_age.length
+  end
+
+
+
 end
--- run.rb
+++ run.rb
@@ -1,9 +1,24 @@
 require_relative "lib/Manager"
 require_relative "lib/Employee"
+
+
 require 'pry'
 
 
 #Test your code here
+m1 = Manager.new("Joseph", "finance", 30)
+m2 = Manager.new("manuel", "recruitment", 49)
+m3 = Manager.new("Tope", "operational", 60)
+m4 = Manager.new("Joshua", "HR", 71)
+m5 = Manager.new("Dapo", "scout", 39)
+
+
+e1 = Employee.new("Mike", 20000, m1)
+e2 = Employee.new("James", 25654, m3)
+e3 = Employee.new("Edwards", 19800, m2)
+e4 = Employee.new("Vicky", 32322, m4)
+e5 = Employee.new("Becky", 98992, m5)
+
 
 
 binding.pry
